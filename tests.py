963a50d0074083cf02a253ef77cef46db5c7ff7a
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-14 12:57:53 -0700

    Added all files for testing-py lab

---
+++ tests.py
@@ -0,0 +1,44 @@
+import unittest
+
+import party
+
+
+class PartyTests(unittest.TestCase):
+    """Tests for my party site."""
+
+    def setUp(self):
+        self.client = party.app.test_client()
+        party.app.config['TESTING'] = True
+
+    def test_homepage(self):
+        result = self.client.get("/")
+        self.assertIn("I'm having a party", result.data)
+
+    def test_no_rsvp_yet(self):
+        # FIXME: Add a test to show we haven't RSVP'd yet
+        result = self.client.get("/")
+
+        self.assertNotIn('<h2>Party Details</h2>', result.data)
+        self.assertIn('<h2>Please RSVP</h2>', result.data)
+
+    def test_rsvp(self):
+        result = self.client.post("/rsvp",
+                                  data={'name': "Jane",
+                                        'email': "jane@jane.com"},
+                                  follow_redirects=True)
+
+        self.assertIn('<h2>Party Details</h2>', result.data)
+        self.assertNotIn('<h2>Please RSVP</h2>', result.data)
+
+    def test_rsvp_mel(self):
+        result = self.client.post("/rsvp",
+                                  data={'name': "Mel Melitpolski",
+                                        'email': "mel@ubermelon.com"},
+                                  follow_redirects=True)
+
+        self.assertNotIn('<h2>Party Details</h2>', result.data)
+        self.assertIn('<h2>Please RSVP</h2>', result.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
